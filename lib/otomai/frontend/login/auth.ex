77924fc2c69220718b54d7b7ca0562f21dbc9133
Blackrush <blackrushx@gmail.com>
2014-12-08 19:46:47 +0100

    Use UserRepo on Frontend

--- lib/otomai/frontend/login/auth.ex
+++ lib/otomai/frontend/login/auth.ex
@@ -1,12 +1,45 @@
 defmodule Otomai.Frontend.Login.Auth do
   use Otomai.Frontend.Base
 
+  alias Otomai.Backend
   alias Otomai.Frontend.Login.Realm
 
   def handle(conn, msg) do
     [username, password] = String.split(msg, "\n#1", parts: 2)
 
+    user = Backend.find_user!(username: username) |> authenticate!(conn, password)
+
     conn |> Conn.set_behaviour(Realm)
-         |> Conn.assign(:user, %{username: username, password: password})
+         |> Conn.assign(:user, user)
+  end
+
+  defp authenticate!(user, conn, password) do
+    unless is_valid_password?(user, password, conn.assigns.ticket) do
+      raise "invalid password"
+    end
+
+    if User.cannot?(user, :authenticate) do
+      raise "cannot authenticate"
+    end
+
+    user
+  end
+
+  defp is_valid_password?(user, password, ticket) do
+    password |> decode_password(ticket)
+             |> cipher(user.salt)
+             |> compare_passwords(user.password)
+  end
+
+  defp decode_password(password, ticket) do
+    raise "todo"
+  end
+
+  defp cipher(password, salt) do
+    raise "todo"
+  end
+
+  defp compare_passwords(left, right) do
+    raise "todo"
   end
 end
