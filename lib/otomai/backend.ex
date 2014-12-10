c6864fefdbfb0d06104efc315a9a48b38f543a65
Blackrush <blackrushx@gmail.com>
2014-12-10 18:50:19 +0100

    Put helper functions into Otomai.Backend module

--- lib/otomai/backend.ex
+++ lib/otomai/backend.ex
@@ -1,6 +1,7 @@
 defmodule Otomai.Backend do
   alias Otomai.User
   alias Otomai.Backend.UserRepo
+  alias Otomai.Backend.RealmRegistry
 
   def find_user([id: id]) do
     UserRepo.find_by_id(UserRepo, id)
@@ -8,6 +9,14 @@ defmodule Otomai.Backend do
 
   def find_user([username: username]) do
     UserRepo.find_by_username(UserRepo, username)
+  end
+
+  def find_realm([id: id]) do
+    RealmRegistry.find_by_id(RealmRegistry, id)
+  end
+
+  def find_realms do
+    RealmRegistry.find_all(RealmRegistry)
   end
 
   def insert(user = %User{}) do
